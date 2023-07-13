150c67ec8a95b8439a115462d2c8e1dbe14675b1
RoAlfonsin <130806097+RoAlfonsin@users.noreply.github.com>
2023-07-13 18:52:15 +0000

    people card created

---
+++ src/js/component/infoCards/peopleCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import lukeImage from "../../../img/people/luke.jpg";
+
+function PeopleCard(){
+
+    const listElementsClasses = "list-group-item p-0 border-0 text-start";
+
+    const peopleInfo = {
+        name: "Luke Skywalker",
+        gender: "Male",
+        eye_color: "Blue",
+        height: "172"
+    };
+
+    return(
+        <div className="card m-2" style={{width: "18rem"}}>
+            <img src={lukeImage} className="card-img-top"/>
+            <div className="card-body">
+                <h5 className="card-title">{peopleInfo.name}</h5>
+                <ul className="list-group">
+                    <li className={listElementsClasses}>Gender: {peopleInfo.gender}</li>
+                    <li className={listElementsClasses}>Eye Color: {peopleInfo.eye_color}</li>
+                    <li className={listElementsClasses}>Height: {peopleInfo.height}</li>
+                </ul>
+            </div>
+            <div className="card-footer d-flex justify-content-around">
+                <button type="button" className="btn btn-outline-primary">Learn More</button>
+                <button type="button" className="btn btn-outline-warning fw-bold">♡</button>
+            </div>
+        </div>
+    );
+};
+
+export default PeopleCard;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,9 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+
+import PeopleCard from "../component/infoCards/peopleCard.jsx";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<PeopleCard/>
 	</div>
 );
