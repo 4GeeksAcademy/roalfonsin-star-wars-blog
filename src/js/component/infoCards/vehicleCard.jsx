f4357bf8b72891c7e73e988257bc4b7683a23e6f
RoAlfonsin <130806097+RoAlfonsin@users.noreply.github.com>
2023-07-13 19:59:23 +0000

    Infocards Created

--- src/js/component/infoCards/vehicleCard.jsx
+++ src/js/component/infoCards/vehicleCard.jsx
@@ -6,7 +6,7 @@ function VehicleCard(){
 
     const listElementsClasses = "list-group-item p-0 border-0 text-start";
 
-    const peopleInfo = {
+    const vehicleInfo = {
         name: "Sand Crawler",
         vehicle_class: "assault walker",
         crew: "1",
@@ -17,11 +17,11 @@ function VehicleCard(){
         <div className="card m-2" style={{width: "18rem"}}>
             <img src={vehicleImage} className="card-img-top"/>
             <div className="card-body">
-                <h5 className="card-title">{peopleInfo.name}</h5>
+                <h5 className="card-title">{vehicleInfo.name}</h5>
                 <ul className="list-group">
-                    <li className={listElementsClasses}>Vehicle Class: {peopleInfo.vehicle_class}</li>
-                    <li className={listElementsClasses}>Crew: {peopleInfo.crew}</li>
-                    <li className={listElementsClasses}>Passengers: {peopleInfo.passengers}</li>
+                    <li className={listElementsClasses}>Vehicle Class: {vehicleInfo.vehicle_class}</li>
+                    <li className={listElementsClasses}>Crew: {vehicleInfo.crew}</li>
+                    <li className={listElementsClasses}>Passengers: {vehicleInfo.passengers}</li>
                 </ul>
             </div>
             <div className="card-footer d-flex justify-content-around">
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import PeopleCard from "../component/infoCards/peopleCard.jsx";
 import VehicleCard from "../component/infoCards/vehicleCard.jsx";
+import PlanetCard from "../component/infoCards/planetCard.jsx";
 
 export const Home = () => (
 	<div className="text-center container-fluid">
 		<div className="row">
 			<PeopleCard/>
 			<VehicleCard/>
+			<PlanetCard/>
 		</div>
 	</div>
 );
